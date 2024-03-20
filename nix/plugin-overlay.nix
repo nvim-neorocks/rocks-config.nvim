448fbd644fa4e18bc67864415d283315d858d260
Marc Jakobi <marc.jakobi@tiko.energy>
2024-03-20 02:00:25 +0100

    fix: use new `rocks.hooks.preload` API to load configs

---
+++ nix/plugin-overlay.nix
@@ -0,0 +1,42 @@
+{
+  name,
+  self,
+}: final: prev: let
+  luaPackage-override = luaself: luaprev: {
+    rocks-config-nvim = luaself.callPackage ({
+      luaOlder,
+      buildLuarocksPackage,
+      lua,
+      rocks-nvim,
+    }:
+      buildLuarocksPackage {
+        pname = name;
+        version = "scm-1";
+        knownRockspec = "${self}/rocks-config.nvim-scm-1.rockspec";
+        src = self;
+        disabled = luaOlder "5.1";
+        propagatedBuildInputs = [
+          rocks-nvim
+        ];
+      }) {};
+  };
+  lua5_1 = prev.lua5_1.override {
+    packageOverrides = luaPackage-override;
+  };
+  lua51Packages = final.lua5_1.pkgs;
+in {
+  inherit
+    lua5_1
+    lua51Packages
+    ;
+
+  vimPlugins =
+    prev.vimPlugins
+    // {
+      rocks-config-nvim = final.neovimUtils.buildNeovimPlugin {
+        pname = name;
+        version = "dev";
+        src = self;
+      };
+    };
+}
