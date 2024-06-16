3e12dabc547b90050940e3befe4c533201bf12db
Marc Jakobi <marc.jakobi@tiko.energy>
2024-06-16 19:32:57 +0200

    build: add basic nix devShell infrastructure

--- flake.nix
+++ flake.nix
@@ -1,44 +1,23 @@
 {
-  description = "Use rocks.nvim to install plugins from git!";
-
-  nixConfig = {
-    extra-substituters = "https://neorocks.cachix.org";
-    extra-trusted-public-keys = "neorocks.cachix.org-1:WqMESxmVTOJX7qoBC54TwrMMoVI1xAM+7yFin8NRfwk=";
-  };
+  description = "devShell for Neovim Lua plugins";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-
-    neorocks.url = "github:nvim-neorocks/neorocks";
-
-    gen-luarc.url = "github:mrcjkb/nix-gen-luarc-json";
-
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     rocks-nvim-input.url = "github:nvim-neorocks/rocks.nvim";
-
     flake-parts.url = "github:hercules-ci/flake-parts";
-
-    pre-commit-hooks = {
-      url = "github:cachix/pre-commit-hooks.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    neorocks.url = "github:nvim-neorocks/neorocks";
+    gen-luarc.url = "github:mrcjkb/nix-gen-luarc-json";
   };
 
   outputs = inputs @ {
     self,
     nixpkgs,
-    neorocks,
-    gen-luarc,
     rocks-nvim-input,
     flake-parts,
-    pre-commit-hooks,
+    neorocks,
+    gen-luarc,
     ...
-  }: let
-    name = "rocks-config.nvim";
-
-    plugin-overlay = import ./nix/plugin-overlay.nix {
-      inherit name self;
-    };
-  in
+  }:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = [
         "x86_64-linux"
@@ -49,6 +28,7 @@
         config,
         self',
         inputs',
+        pkgs,
         system,
         ...
       }: let
@@ -58,72 +38,30 @@
             neorocks.overlays.default
             gen-luarc.overlays.default
             rocks-nvim-input.overlays.default
-            plugin-overlay
           ];
         };
-
         luarc = pkgs.mk-luarc {
           nvim = pkgs.neovim-nightly;
           neodev-types = "nightly";
           plugins = with pkgs.lua51Packages; [
             rocks-nvim
+            nvim-nio
           ];
         };
-
-        type-check-nightly = pre-commit-hooks.lib.${system}.run {
-          src = self;
-          hooks = {
-            lua-ls.enable = true;
-          };
-          settings = {
-            lua-ls.config = luarc;
-          };
-        };
-
-        pre-commit-check = pre-commit-hooks.lib.${system}.run {
-          src = self;
-          hooks = {
-            alejandra.enable = true;
-            stylua.enable = true;
-            luacheck.enable = true;
-            editorconfig-checker.enable = true;
-          };
-        };
-
-        devShell = pkgs.lua51Packages.rocks-config-nvim.overrideAttrs (oa: {
-          name = "rocks-config.nvim devShell";
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "lua devShell";
           shellHook = ''
-            ${pre-commit-check.shellHook}
             ln -fs ${pkgs.luarc-to-json luarc} .luarc.json
           '';
-          buildInputs = with pre-commit-hooks.packages.${system};
-            [
-              alejandra
-              lua-language-server
-              stylua
-              luacheck
-              editorconfig-checker
-            ]
-            ++ oa.buildInputs;
-        });
-      in {
-        packages.default = pkgs.lua5_1.pkgs.rocks-config-nvim;
-
-        devShells = {
-          default = devShell;
-          inherit devShell;
+          buildInputs = with pkgs; [
+            lua-language-server
+            stylua
+            lua51Packages.luacheck
+            lua5_1
+            luarocks
+          ];
         };
-
-        # TODO: add type-check-stable when ready
-        checks = {
-          inherit
-            pre-commit-check
-            type-check-nightly
-            ;
-        };
-      };
-      flake = {
-        overlays.default = plugin-overlay;
       };
     };
 }
